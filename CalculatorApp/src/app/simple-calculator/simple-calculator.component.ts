c3b385b9ebac2604e2c14c6b0a799d07b4882a54
Austin <aandrews@brasfieldgorrie.com>
2020-05-01 18:31:27 -0500

    Add logic for calculator. Now works as desired.

--- CalculatorApp/src/app/simple-calculator/simple-calculator.component.ts
+++ CalculatorApp/src/app/simple-calculator/simple-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserStack } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-simple-calculator',
@@ -8,12 +9,73 @@ import { Component, OnInit } from '@angular/core';
 export class SimpleCalculatorComponent implements OnInit {
 
   constructor() { }
-  
+
   DisplayValue: number;
+  FirstValue: number;
+  SecondValue: number;
+  Operation: string;
 
   ngOnInit(): void {
-    this.DisplayValue = 0;
-  }
+    this.ResetState();
   }
 
+  Negate(): void {
+    this.DisplayValue = this.DisplayValue * -1;
+  }
+
+  ClickNumber(number: number): void {
+    this.DisplayValue = 10 * this.DisplayValue + number;
+    if (this.Operation == null) {
+      this.FirstValue = this.DisplayValue;
+    }
+    else {
+      this.SecondValue = this.DisplayValue;
+    }
+  }
+
+  ClickOperation(operation: string): void {
+    this.PerformOperation();
+    this.FirstValue = this.DisplayValue;
+    this.Operation = operation;
+    this.DisplayValue = 0;
+  }
+
+  ClickEquals(): void {
+    this.PerformOperation();
+  }
+
+  ResetState(): void {
+    this.DisplayValue = 0;
+    this.FirstValue = this.DisplayValue;
+    this.SecondValue = null;
+    this.Operation = null;
+  }
+
+  PerformOperation(): void {
+    if (this.CanPerformOperation()) {
+      switch (this.Operation) {
+        case '+':
+          this.DisplayValue = this.FirstValue + this.SecondValue;
+          break;
+        case '-':
+          this.DisplayValue = this.FirstValue - this.SecondValue;
+          break;
+        case 'x':
+          this.DisplayValue = this.FirstValue * this.SecondValue;
+          break;
+        case '/':
+          this.DisplayValue = this.FirstValue / this.SecondValue;
+          break;
+        default:
+          return;
+      }
+      this.FirstValue = this.DisplayValue;
+      this.Operation = null;
+      this.SecondValue = null;
+    }
+  }
+
+  CanPerformOperation(): boolean {
+    return this.Operation != null && this.FirstValue != null && this.SecondValue != null;
+  }
 }
